82d8e5dab8475fa283cd05e9748f488596fb28cf
Kaluchii <mr.kaluchii@mail.ru>
2017-04-09 23:56:39 +0600

    Header and 2 blocks complete

--- resources/views/front/header.blade.php
+++ resources/views/front/header.blade.php
@@ -1,6 +1,19 @@
 @section('header')
     <header class="header">
-        <div class="header__wrap-1220">
+        <div class="header__wrap-1170">
+            <div class="header__logo-wrap">
+                <img src="/img/logo.png" alt="" class="header__logo">
+            </div>
+            <div class="header__inf-wrap">
+                <div class="header__inf">
+                    <img src="/img/40x60.jpg" alt="" class="header__inf-img">
+                    <p class="header__inf-text">При поддержке Стритбольной Лиги Казахстана</p>
+                </div>
+                <div class="header__inf">
+                    <img src="/img/70x40.jpg" alt="" class="header__inf-img">
+                    <p class="header__inf-text header__inf-text--longer">Турнир соответствует международному регламенту FIBA 3X3</p>
+                </div>
+            </div>
         </div>
     </header>
 @endsection
--- resources/views/front/index/index.blade.php
+++ resources/views/front/index/index.blade.php
@@ -1,42 +1,50 @@
 @extends('front.layout')
 @include('front.index.meta')
 @section('content')
-    <section class="about">
-        <div class="about__facts-wrap facts">
-            <div class="facts__hand-wrap">
-                <div class="facts__hand">
-                    <img src="/img/hand-with-lazer.png" alt="" class="facts__hand-img">
-                    <img src="/img/laser_on.png" alt="" class="facts__hand-laser-img">
+    <section class="titular">
+        <div class="titular__wrap-1150">
+            <h1 class="titular__block-title block-title">Крупнейший в Казахстане любительский турнир
+                <br><span class="thin-green"> по баскетболу 3x3</span>
+            </h1>
+            <div class="titular__media-wrap media">
+                <img src="/img/top.jpg" alt="" class="media__img">
+            </div>
+            <div class="titular__facts facts">
+                <p class="facts__item">10 ЛЕТ В ДЕЛЕ<br><span class="thin-green"> НА ВЫСОКОМ УРОВНЕ</span></p>
+                <p class="facts__item">БОЛЕЕ 150 КОМАНД<br><span class="thin-green"> И ТЫСЯЧИ ЗРИТЕЛЕЙ</span></p>
+                <p class="facts__item"><span class="thin-green">4 ВЫХОДНЫХ ДНЯ<br></span> 9 ДНЕЙ АКТИВНОСТИ</p>
+                <p class="facts__item">УЧАСТНИКИ ИЗ<span class="thin-green"> США,<br>СРЕДНЕЙ АЗИИ И СО ВСЕГО КАЗАХСТАНА</span></p>
+            </div>
+        </div>
+    </section>
+    <section class="statistic">
+        <div class="statistic__wrap-1090">
+            <h2 class="statistic__block-title block-title">СТАТИСТИКА<span class="thin-green"> ТУРНИРА</span></h2>
+            <div class="statistic__img-facts-wrap">
+                <img src="/img/820x540.jpg" alt="" class="statistic__img">
+                <div class="statistic__facts facts-in-figures">
+                    <div class="facts-in-figures__item">
+                        <p class="facts-in-figures__figure">700</p>
+                        <p class="facts-in-figures__text">УЧАСТНИКОВ</p>
+                    </div>
+                    <div class="facts-in-figures__item">
+                        <p class="facts-in-figures__figure">80 000</p>
+                        <p class="facts-in-figures__text">ЗРИТЕЛЕЙ<br>И БОЛЕЛЬЩИКИ</p>
+                    </div>
+                    <div class="facts-in-figures__item">
+                        <p class="facts-in-figures__figure">8</p>
+                        <p class="facts-in-figures__text">ГОРОДОВ</p>
+                    </div>
+                    <div class="facts-in-figures__item">
+                        <p class="facts-in-figures__figure">50 000</p>
+                        <p class="facts-in-figures__text">ЗРИТЕЛЕЙ В СОЦИАЛЬНЫХ СЕТЯХ</p>
+                    </div>
                 </div>
             </div>
-            <div class="facts__col-1-2">
-                <h2 class="about__title">О компании</h2>
-                <div class="facts__item fact">
-                    <img src="/img/1a.jpg" alt="" class="fact__img">
-                    <h3 class="fact__title">Собственное производство</h3>
-                    <p class="fact__description">Мы сами выбираем и заказываем комплектующие с заводов Китая, а сборнкой
-                        занимаются квалифицированные и ответственные сотрудники.</p>
-                </div>
-                <div class="facts__reputation">
-                    <img src="/img/reputation.png" alt="Мы дорожим репутацией" class="facts__img">
-                    <p class="facts__reputation-text">Мы бережем репутацию и хотим быть
-                        компанией, на которую вы рассчитываете.</p>
-                </div>
-            </div>
-            <div class="facts__col-1-2">
-                <div class="facts__item fact">
-                    <img src="/img/2a.jpg" alt="" class="fact__img">
-                    <h3 class="fact__title">Опыт интеграции</h3>
-                    <p class="fact__description">Мы понимаем особенности автоматизации бизнеса. Это позволяет нам решить
-                        многие проблемы еще до того, как они появятся.</p>
-                </div>
-                <div class="facts__item fact">
-                    <img src="/img/3a.jpg" alt="" class="fact__img">
-                    <h3 class="fact__title">Лицом к партнерам</h3>
-                    <p class="fact__description">Нашим партнерам не нужно выполнять план продаж, дожидаться очереди или
-                        заказывать не нужный объем. Мы стремимся к честному и прозрачному сотрудничеству.</p>
-                </div>
-            </div>
+        </div>
+        <div class="statistic__sj-grow">
+            <p class="statistic__sj-grow-text">За 10 лет, SummerJam вырос из небольшого турнира в парке в самый
+            крупный и стабильный любительский турнир Казахстана.<br>И мы только начали.</p>
         </div>
     </section>
 @endsection
