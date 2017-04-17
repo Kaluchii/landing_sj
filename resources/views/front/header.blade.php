d29e55d6bf4bb08e59ce82499b32dc1c34e167de
Kaluchii <mr.kaluchii@mail.ru>
2017-04-17 18:09:19 +0600

    Complete blocks: statistic, principles, objectives, leaders, support

--- resources/views/front/header.blade.php
+++ resources/views/front/header.blade.php
@@ -6,6 +6,7 @@
             </div>
             <div class="header__inf-wrap">
                 <div class="header__inf">
+                    Поставить тени картинкам
                     <img src="/img/40x60.jpg" alt="" class="header__inf-img">
                     <p class="header__inf-text">При поддержке Стритбольной Лиги Казахстана</p>
                 </div>
--- resources/views/front/index/index.blade.php
+++ resources/views/front/index/index.blade.php
@@ -2,21 +2,24 @@
 @include('front.index.meta')
 @section('content')
     <section class="titular">
-        <div class="titular__wrap-1150">
-            <h1 class="titular__block-title block-title">Крупнейший в Казахстане любительский турнир
+        <div class="titular__wrap-1220">
+            <h1 class="titular__block-title">Крупнейший в Казахстане любительский турнир
                 <br><span class="thin-green"> по баскетболу 3x3</span>
             </h1>
             <div class="titular__media-wrap media">
-                <img src="/img/top.jpg" alt="" class="media__img">
+                <img src="/img/1220x530.jpg" alt="" class="media__img">
             </div>
             <div class="titular__facts facts">
-                <p class="facts__item">10 ЛЕТ В ДЕЛЕ<br><span class="thin-green"> НА ВЫСОКОМ УРОВНЕ</span></p>
-                <p class="facts__item">БОЛЕЕ 150 КОМАНД<br><span class="thin-green"> И ТЫСЯЧИ ЗРИТЕЛЕЙ</span></p>
-                <p class="facts__item"><span class="thin-green">4 ВЫХОДНЫХ ДНЯ<br></span> 9 ДНЕЙ АКТИВНОСТИ</p>
-                <p class="facts__item">УЧАСТНИКИ ИЗ<span class="thin-green"> США,<br>СРЕДНЕЙ АЗИИ И СО ВСЕГО КАЗАХСТАНА</span></p>
+                <p class="facts__item">10 ЛЕТ В ДЕЛЕ<br><span class="reg-green"> НА ВЫСОКОМ УРОВНЕ</span></p>
+                <p class="facts__item">БОЛЕЕ 150 КОМАНД<br><span class="reg-green"> И ТЫСЯЧИ ЗРИТЕЛЕЙ</span></p>
+                <p class="facts__item"><span class="reg-green">4 ВЫХОДНЫХ ДНЯ<br></span> 9 ДНЕЙ АКТИВНОСТИ</p>
+                <p class="facts__item">УЧАСТНИКИ ИЗ<span
+                            class="reg-green"> США,<br>СРЕДНЕЙ АЗИИ И СО ВСЕГО КАЗАХСТАНА</span></p>
             </div>
         </div>
     </section>
+
+
     <section class="statistic">
         <div class="statistic__wrap-1090">
             <h2 class="statistic__block-title block-title">СТАТИСТИКА<span class="thin-green"> ТУРНИРА</span></h2>
@@ -44,7 +47,125 @@
         </div>
         <div class="statistic__sj-grow">
             <p class="statistic__sj-grow-text">За 10 лет, SummerJam вырос из небольшого турнира в парке в самый
-            крупный и стабильный любительский турнир Казахстана.<br>И мы только начали.</p>
+                крупный и стабильный любительский турнир Казахстана.<br>И мы только начали.</p>
         </div>
+    </section>
+
+
+    <section class="principles">
+        <div class="principles__wrap-1080">
+            <h2 class="principles__block-title block-title">НАШИ<span class="thin-green"> ПРИНЦИПЫ</span></h2>
+            <div class="principles__list three-elem-list">
+                <div class="three-elem-list__item info-block">
+                    <img src="/img/280x280.jpg" alt="" class="info-block__img">
+                    <h3 class="info-block__title">ЕСЛИ МЫ ДЕЛАЕМ — <br><span
+                                class="reg-green">МЫ ДЕЛАЕМ ЭТО ХОРОШО</span></h3>
+                    <p class="info-block__text">Наш главный принцип — сохранить
+                        честность, интерес и любовь
+                        участников. Никаких подделок: все
+                        будет на высоком уровне.</p>
+                </div>
+                <div class="three-elem-list__item info-block">
+                    <img src="/img/280x280.jpg" alt="" class="info-block__img">
+                    <h3 class="info-block__title">НЕ РАДИ <br><span class="reg-green">ДЕНЕГ</span></h3>
+                    <p class="info-block__text">Мы не пытаемся заработать на
+                        баскетболе — мы его развиваем.
+                        Все средства, полученные от
+                        турнира, возвращаются в развитие
+                        баскетбола.</p>
+                </div>
+                <div class="three-elem-list__item info-block">
+                    <img src="/img/280x280.jpg" alt="" class="info-block__img">
+                    <h3 class="info-block__title">ШАГ ВПЕРЕД <br><span class="reg-green">КАЖДЫЙ ГОД</span></h3>
+                    <p class="info-block__text">Каждый турнир — зимой или летом,
+                        мы делаем шаг вперед и поднимаем
+                        организацию тунрира на новый
+                        уровень.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="objectives">
+        <div class="objectives__wrap-1180">
+            <h2 class="objectives__block-title block-title">ЦЕЛИ<span class="thin-green"> ТУРНИРА</span></h2>
+            <div class="objectives__img-targets-wrap">
+                <img src="/img/810x530.jpg" alt="" class="objectives__img">
+                <div class="objectives__list targets-list">
+                    <div class="targets-list__item info-block">
+                        <h3 class="info-block__title"><span class="reg-green">РАЗВИТИЕ</span> СПОРТИВНОЙ ЭТИКИ И
+                            КУЛЬТУРЫ</h3>
+                        <p class="info-block__text">Не смотря на соревновательный дух и огромную самоотдачу в игре, мы
+                            стремимся налаживать дружеские отношения в сообществе и сближать игроков.</p>
+                    </div>
+                    <div class="targets-list__item info-block">
+                        <h3 class="info-block__title"><span class="reg-green">ПРОГРЕСС</span><br> БАСКЕТБОЛА</h3>
+                        <p class="info-block__text">Баскетбол меняет культуру и учит людей действовать вместе. Нас
+                            вдохновляет пример Сербии и Литвы: постоянных участников и призеров Олимпиады. Мы верим,
+                            Казахстан может не хуже и для этого не нужно ждать: нужно действовать.</p>
+                    </div>
+                    <div class="targets-list__item info-block">
+                        <h3 class="info-block__title"><span class="reg-green">ЛИДЕРСТВО</span><br> ПРИМЕРОМ</h3>
+                        <p class="info-block__text">Мы привлекаем волонтеров, организации и спонсоров, чтобы подать
+                            молодежи пример ответственной и деятельной команды, которая не ждет изменений, но сама
+                            делает их возможными.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="leaders">
+        <div class="leaders__wrap-1080">
+            <h2 class="leaders__block-title block-title"><span class="thin-green">НАШИ</span> ЛИДЕРЫ</h2>
+            <div class="leaders__list three-elem-list">
+                <div class="three-elem-list__item info-block">
+                    <img src="/img/280x280.jpg" alt="" class="info-block__img">
+                    <h3 class="info-block__title">ДЕМИАН ЛОНГ <br><span class="reg-green">БОСТОН, США</span></h3>
+                    <p class="info-block__text">Наш главный принцип — сохранить
+                        честность, интерес и любовь
+                        участников. Никаких подделок: все
+                        будет на высоком уровне.</p>
+                </div>
+                <div class="three-elem-list__item info-block">
+                    <img src="/img/280x280.jpg" alt="" class="info-block__img">
+                    <h3 class="info-block__title">ЖОМАРТ АССЕНКАНОВ <br><span class="reg-green">КАЗАХСТАН, АЛМАТЫ</span></h3>
+                    <p class="info-block__text">Мы не пытаемся заработать на
+                        баскетболе — мы его развиваем.
+                        Все средства, полученные от
+                        турнира, возвращаются в развитие
+                        баскетбола.</p>
+                </div>
+                <div class="three-elem-list__item info-block">
+                    <img src="/img/280x280.jpg" alt="" class="info-block__img">
+                    <h3 class="info-block__title">ЛЮДМИЛА ПОНКРАШКИНА <br><span class="reg-green">КАЗАХСТАН, АЛМАТЫ</span></h3>
+                    <p class="info-block__text">Каждый турнир — зимой или летом,
+                        мы делаем шаг вперед и поднимаем
+                        организацию тунрира на новый
+                        уровень.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="support">
+        <div class="support__wrap-1080">
+            <h3 class="support__block-title"><span class="reg-green">ИНФОРМАЦИОННАЯ</span> ПОДДЕРЖКА</h3>
+            <div class="support__images-list">
+                <img src="/img/200x130.jpg" alt="" class="support__img">
+                <img src="/img/200x130.jpg" alt="" class="support__img">
+                <img src="/img/200x130.jpg" alt="" class="support__img">
+                <img src="/img/200x130.jpg" alt="" class="support__img">
+                <img src="/img/200x130.jpg" alt="" class="support__img">
+            </div>
+        </div>
+    </section>
+
+
+    <section class="plans">
+
     </section>
 @endsection
