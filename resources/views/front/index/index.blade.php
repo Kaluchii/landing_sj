82979b67e40fe4a6d85301b213eaf06b57a9d80f
Kaluchii <mr.kaluchii@mail.ru>
2017-04-18 17:47:14 +0600

    Complete blocks: plans, information

--- resources/views/front/index/index.blade.php
+++ resources/views/front/index/index.blade.php
@@ -9,13 +9,14 @@
             <div class="titular__media-wrap media">
                 <img src="/img/1220x530.jpg" alt="" class="media__img">
             </div>
-            <div class="titular__facts facts">
-                <p class="facts__item">10 ЛЕТ В ДЕЛЕ<br><span class="reg-green"> НА ВЫСОКОМ УРОВНЕ</span></p>
-                <p class="facts__item">БОЛЕЕ 150 КОМАНД<br><span class="reg-green"> И ТЫСЯЧИ ЗРИТЕЛЕЙ</span></p>
-                <p class="facts__item"><span class="reg-green">4 ВЫХОДНЫХ ДНЯ<br></span> 9 ДНЕЙ АКТИВНОСТИ</p>
-                <p class="facts__item">УЧАСТНИКИ ИЗ<span
-                            class="reg-green"> США,<br>СРЕДНЕЙ АЗИИ И СО ВСЕГО КАЗАХСТАНА</span></p>
-            </div>
+            <ul class="titular__facts facts">
+                <li class="facts__item">10&nbsp;ЛЕТ В ДЕЛЕ<br><span class="reg-green"> НА ВЫСОКОМ УРОВНЕ</span></li>
+                <li class="facts__item">БОЛЕЕ 150 КОМАНД<br><span class="reg-green"> И ТЫСЯЧИ ЗРИТЕЛЕЙ</span></li>
+                <li class="facts__item"><span class="reg-green">4&nbsp;ВЫХОДНЫХ ДНЯ<br></span> 9&nbsp;ДНЕЙ АКТИВНОСТИ
+                </li>
+                <li class="facts__item">УЧАСТНИКИ ИЗ<span
+                            class="reg-green"> США,<br>СРЕДНЕЙ АЗИИ И СО ВСЕГО КАЗАХСТАНА</span></li>
+            </ul>
         </div>
     </section>
 
@@ -25,24 +26,24 @@
             <h2 class="statistic__block-title block-title">СТАТИСТИКА<span class="thin-green"> ТУРНИРА</span></h2>
             <div class="statistic__img-facts-wrap">
                 <img src="/img/820x540.jpg" alt="" class="statistic__img">
-                <div class="statistic__facts facts-in-figures">
-                    <div class="facts-in-figures__item">
+                <ul class="statistic__facts facts-in-figures">
+                    <li class="facts-in-figures__item">
                         <p class="facts-in-figures__figure">700</p>
                         <p class="facts-in-figures__text">УЧАСТНИКОВ</p>
-                    </div>
-                    <div class="facts-in-figures__item">
-                        <p class="facts-in-figures__figure">80 000</p>
+                    </li>
+                    <li class="facts-in-figures__item">
+                        <p class="facts-in-figures__figure">80&nbsp;000</p>
                         <p class="facts-in-figures__text">ЗРИТЕЛЕЙ<br>И БОЛЕЛЬЩИКИ</p>
-                    </div>
-                    <div class="facts-in-figures__item">
+                    </li>
+                    <li class="facts-in-figures__item">
                         <p class="facts-in-figures__figure">8</p>
                         <p class="facts-in-figures__text">ГОРОДОВ</p>
-                    </div>
-                    <div class="facts-in-figures__item">
-                        <p class="facts-in-figures__figure">50 000</p>
+                    </li>
+                    <li class="facts-in-figures__item">
+                        <p class="facts-in-figures__figure">50&nbsp;000</p>
                         <p class="facts-in-figures__text">ЗРИТЕЛЕЙ В СОЦИАЛЬНЫХ СЕТЯХ</p>
-                    </div>
-                </div>
+                    </li>
+                </ul>
             </div>
         </div>
         <div class="statistic__sj-grow">
@@ -55,8 +56,8 @@
     <section class="principles">
         <div class="principles__wrap-1080">
             <h2 class="principles__block-title block-title">НАШИ<span class="thin-green"> ПРИНЦИПЫ</span></h2>
-            <div class="principles__list three-elem-list">
-                <div class="three-elem-list__item info-block">
+            <ul class="principles__list three-elem-list">
+                <li class="three-elem-list__item info-block">
                     <img src="/img/280x280.jpg" alt="" class="info-block__img">
                     <h3 class="info-block__title">ЕСЛИ МЫ ДЕЛАЕМ — <br><span
                                 class="reg-green">МЫ ДЕЛАЕМ ЭТО ХОРОШО</span></h3>
@@ -64,8 +65,8 @@
                         честность, интерес и любовь
                         участников. Никаких подделок: все
                         будет на высоком уровне.</p>
-                </div>
-                <div class="three-elem-list__item info-block">
+                </li>
+                <li class="three-elem-list__item info-block">
                     <img src="/img/280x280.jpg" alt="" class="info-block__img">
                     <h3 class="info-block__title">НЕ РАДИ <br><span class="reg-green">ДЕНЕГ</span></h3>
                     <p class="info-block__text">Мы не пытаемся заработать на
@@ -73,16 +74,16 @@
                         Все средства, полученные от
                         турнира, возвращаются в развитие
                         баскетбола.</p>
-                </div>
-                <div class="three-elem-list__item info-block">
+                </li>
+                <li class="three-elem-list__item info-block">
                     <img src="/img/280x280.jpg" alt="" class="info-block__img">
                     <h3 class="info-block__title">ШАГ ВПЕРЕД <br><span class="reg-green">КАЖДЫЙ ГОД</span></h3>
                     <p class="info-block__text">Каждый турнир — зимой или летом,
                         мы делаем шаг вперед и поднимаем
                         организацию тунрира на новый
                         уровень.</p>
-                </div>
-            </div>
+                </li>
+            </ul>
         </div>
     </section>
 
@@ -92,26 +93,26 @@
             <h2 class="objectives__block-title block-title">ЦЕЛИ<span class="thin-green"> ТУРНИРА</span></h2>
             <div class="objectives__img-targets-wrap">
                 <img src="/img/810x530.jpg" alt="" class="objectives__img">
-                <div class="objectives__list targets-list">
-                    <div class="targets-list__item info-block">
+                <ul class="objectives__list targets-list">
+                    <li class="targets-list__item info-block">
                         <h3 class="info-block__title"><span class="reg-green">РАЗВИТИЕ</span> СПОРТИВНОЙ ЭТИКИ И
                             КУЛЬТУРЫ</h3>
                         <p class="info-block__text">Не смотря на соревновательный дух и огромную самоотдачу в игре, мы
                             стремимся налаживать дружеские отношения в сообществе и сближать игроков.</p>
-                    </div>
-                    <div class="targets-list__item info-block">
+                    </li>
+                    <li class="targets-list__item info-block">
                         <h3 class="info-block__title"><span class="reg-green">ПРОГРЕСС</span><br> БАСКЕТБОЛА</h3>
                         <p class="info-block__text">Баскетбол меняет культуру и учит людей действовать вместе. Нас
                             вдохновляет пример Сербии и Литвы: постоянных участников и призеров Олимпиады. Мы верим,
                             Казахстан может не хуже и для этого не нужно ждать: нужно действовать.</p>
-                    </div>
-                    <div class="targets-list__item info-block">
+                    </li>
+                    <li class="targets-list__item info-block">
                         <h3 class="info-block__title"><span class="reg-green">ЛИДЕРСТВО</span><br> ПРИМЕРОМ</h3>
                         <p class="info-block__text">Мы привлекаем волонтеров, организации и спонсоров, чтобы подать
                             молодежи пример ответственной и деятельной команды, которая не ждет изменений, но сама
                             делает их возможными.</p>
-                    </div>
-                </div>
+                    </li>
+                </ul>
             </div>
         </div>
     </section>
@@ -120,33 +121,35 @@
     <section class="leaders">
         <div class="leaders__wrap-1080">
             <h2 class="leaders__block-title block-title"><span class="thin-green">НАШИ</span> ЛИДЕРЫ</h2>
-            <div class="leaders__list three-elem-list">
-                <div class="three-elem-list__item info-block">
+            <ul class="leaders__list three-elem-list">
+                <li class="three-elem-list__item info-block">
                     <img src="/img/280x280.jpg" alt="" class="info-block__img">
                     <h3 class="info-block__title">ДЕМИАН ЛОНГ <br><span class="reg-green">БОСТОН, США</span></h3>
                     <p class="info-block__text">Наш главный принцип — сохранить
                         честность, интерес и любовь
                         участников. Никаких подделок: все
                         будет на высоком уровне.</p>
-                </div>
-                <div class="three-elem-list__item info-block">
+                </li>
+                <li class="three-elem-list__item info-block">
                     <img src="/img/280x280.jpg" alt="" class="info-block__img">
-                    <h3 class="info-block__title">ЖОМАРТ АССЕНКАНОВ <br><span class="reg-green">КАЗАХСТАН, АЛМАТЫ</span></h3>
+                    <h3 class="info-block__title">ЖОМАРТ АССЕНКАНОВ <br><span class="reg-green">КАЗАХСТАН, АЛМАТЫ</span>
+                    </h3>
                     <p class="info-block__text">Мы не пытаемся заработать на
                         баскетболе — мы его развиваем.
                         Все средства, полученные от
                         турнира, возвращаются в развитие
                         баскетбола.</p>
-                </div>
-                <div class="three-elem-list__item info-block">
+                </li>
+                <li class="three-elem-list__item info-block">
                     <img src="/img/280x280.jpg" alt="" class="info-block__img">
-                    <h3 class="info-block__title">ЛЮДМИЛА ПОНКРАШКИНА <br><span class="reg-green">КАЗАХСТАН, АЛМАТЫ</span></h3>
+                    <h3 class="info-block__title">ЛЮДМИЛА ПОНКРАШКИНА <br><span
+                                class="reg-green">КАЗАХСТАН, АЛМАТЫ</span></h3>
                     <p class="info-block__text">Каждый турнир — зимой или летом,
                         мы делаем шаг вперед и поднимаем
                         организацию тунрира на новый
                         уровень.</p>
-                </div>
-            </div>
+                </li>
+            </ul>
         </div>
     </section>
 
@@ -154,18 +157,80 @@
     <section class="support">
         <div class="support__wrap-1080">
             <h3 class="support__block-title"><span class="reg-green">ИНФОРМАЦИОННАЯ</span> ПОДДЕРЖКА</h3>
-            <div class="support__images-list">
-                <img src="/img/200x130.jpg" alt="" class="support__img">
-                <img src="/img/200x130.jpg" alt="" class="support__img">
-                <img src="/img/200x130.jpg" alt="" class="support__img">
-                <img src="/img/200x130.jpg" alt="" class="support__img">
-                <img src="/img/200x130.jpg" alt="" class="support__img">
-            </div>
+            <ul class="support__images-list">
+                <li class="support__images-list-item"><img src="/img/200x130.jpg" alt="" class="support__img"></li>
+                <li class="support__images-list-item"><img src="/img/200x130.jpg" alt="" class="support__img"></li>
+                <li class="support__images-list-item"><img src="/img/200x130.jpg" alt="" class="support__img"></li>
+                <li class="support__images-list-item"><img src="/img/200x130.jpg" alt="" class="support__img"></li>
+                <li class="support__images-list-item"><img src="/img/200x130.jpg" alt="" class="support__img"></li>
+            </ul>
         </div>
     </section>
 
 
     <section class="plans">
-
+        <div class="plans__wrap-1210">
+            <div class="plans__text-col">
+                <h2 class="plans__block-title block-title"><span class="thin-green">ПЛАНЫ</span> РАЗВИТИЯ</h2>
+                <ul class="plans__list two-col-list">
+                    <li class="two-col-list__item info-block">
+                        <h3 class="info-block__title"><span class="reg-green">ОТБОРОЧНЫЕ ТУРНИРЫ</span><br> И БОЛЬШОЙ
+                            ФИНАЛ</h3>
+                        <p class="info-block__text">Мы будем вовлекать больше городов Казахстана: отборочные турниры на
+                            местах и большой, главный турнир в Алматы.</p>
+                    </li>
+                    <li class="two-col-list__item info-block">
+                        <h3 class="info-block__title"><span class="reg-green">БАСКЕТБОЛ</span><br> БЕЗ ОСТАНОВКИ</h3>
+                        <p class="info-block__text">Игроки хотят большего: Джэм давно вырос из летнего турнира. Интерес
+                            к зимнему стритболу показал, что команды хотят больше и мы дадим им такую возможность.</p>
+                    </li>
+                    <li class="two-col-list__item info-block">
+                        <h3 class="info-block__title"><span class="reg-green">УЧАСТНИКИ</span><br> ИЗ ЕВРОПЫ И АЗИИ</h3>
+                        <p class="info-block__text">Мы будем привлекать к участию профессиональные команды с
+                            международных турниров и повышать авторитет казахстанских турниров.</p>
+                    </li>
+                    <li class="two-col-list__item info-block">
+                        <h3 class="info-block__title"><span class="reg-green">АКТИВНАЯ РАБОТА</span><br> СО СМИ</h3>
+                        <p class="info-block__text">Мы хотим активней привлекать спортивные сайты, телеканалы и
+                            муниципальные организации в информационную поддержку турнира.</p>
+                    </li>
+                </ul>
+            </div>
+            <img src="/img/570x650.jpg" alt="" class="plans__img">
+        </div>
     </section>
+
+
+    <section class="information">
+        <div class="information__wrap-1110">
+            <div class="information__text-wrap">
+                <h2 class="information__block-title block-title"><span class="thin-green">ИНФОРМАЦИЯ</span> ДЛЯ СПОНСОРА
+                </h2>
+                <div class="information__text">
+                    <p class="information__text-paragraph">Мы предоставляем статус официального спонсора каждой
+                        компании, поддержавшей турнир. Спонсорские пакеты отличаются содержанием, вы можете выбрать и
+                        дополнить тот, что вам больше подходит.</p><br>
+                    <p class="information__text-paragraph">Каждый спонсорский пакет предусматривает публикации в
+                        социальных сетях и анонсы между играми, конкурсами.</p>
+                </div>
+                <div class="information__give-offer-wrap">
+                    <a href="#" class="information__give-offer-btn">Получить предложение</a>
+                </div>
+            </div>
+            <ul class="information__images-list images-list">
+                <li class="images-list__item"><img src="/img/200x130w.jpg" alt="" class="images-list__img"></li>
+                <li class="images-list__item"><img src="/img/200x130w.jpg" alt="" class="images-list__img"></li>
+                <li class="images-list__item"><img src="/img/200x130w.jpg" alt="" class="images-list__img"></li>
+                <li class="images-list__item"><img src="/img/200x130w.jpg" alt="" class="images-list__img"></li>
+                <li class="images-list__item"><img src="/img/200x130w.jpg" alt="" class="images-list__img"></li>
+                <li class="images-list__item"><img src="/img/200x130w.jpg" alt="" class="images-list__img"></li>
+                <li class="images-list__item"><img src="/img/200x130w.jpg" alt="" class="images-list__img"></li>
+                <li class="images-list__item"><img src="/img/200x130w.jpg" alt="" class="images-list__img"></li>
+                <li class="images-list__item"><img src="/img/200x130w.jpg" alt="" class="images-list__img"></li>
+            </ul>
+        </div>
+    </section>
+
+
+    <section class="packages"></section>
 @endsection
