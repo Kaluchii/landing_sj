f1f553a1c0f674688756100451a3be7a9fafda97
Kaluchii <mr.kaluchii@mail.ru>
2017-04-25 10:56:09 +0600

    Js for moving blocks on different resolutions

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     // Перестройка блоков контента на разных разрешениях
     function MoveBlock() {
-        if ($(window).width() <= '1000') {
-            $('.images-list').prependTo($('.information__text-wrap'));
-            $('.information__block-title').prependTo($('.information__text-wrap'));
+        if ($(window).width() <= 1000) {
+            $('.images-list').insertBefore($('.information__give-offer-wrap'));
         } else {
             $('.images-list').appendTo($('.information__wrap-1110'));
         }
     }
 
-    $(window).on('load resize', MoveBlock());
+    $(window).on('resize', MoveBlock);
+    $(window).on('load', MoveBlock());
 });
