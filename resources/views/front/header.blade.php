4473feec8dda6970dd848803f6326f0865d371de
kaluchii <mr.kaluchii@mail.ru>
2017-06-09 12:40:53 +0600

    Menu fix

--- resources/views/front/header.blade.php
+++ resources/views/front/header.blade.php
@@ -8,22 +8,22 @@
                       </span>
                 </button>
                 <ul class="menu__list main-menu-list">
-                    <li class="menu__item"><a href="/" class="menu__link">О&nbsp;ТУРНИРЕ</a></li>
+                    <li class="menu__item"><a href="/" class="menu__link link">О&nbsp;ТУРНИРЕ</a></li>
                     <li class="menu__item menu__item--delimiter">•</li>
-                    <li class="menu__item"><a href="#" class="menu__link">ВОЛОНТЕРАМ</a></li>
+                    <li class="menu__item"><a href="#" class="menu__link link">ВОЛОНТЕРАМ</a></li>
                     <li class="menu__item menu__item--delimiter">•</li>
-                    <li class="menu__item"><a href="/for-players" class="menu__link">ИГРОКАМ</a></li>
+                    <li class="menu__item"><a href="/for-players" class="menu__link link">ИГРОКАМ</a></li>
                     <li class="menu__item menu__item--delimiter">•</li>
-                    <li class="menu__item"><a href="#" class="menu__link">ВИДЕО</a></li>
+                    <li class="menu__item"><a href="#" class="menu__link link">ВИДЕО</a></li>
                     <li class="menu__item menu__item--delimiter">•</li>
-                    <li class="menu__item"><a href="#" class="menu__link">ФОТО</a></li>
+                    <li class="menu__item"><a href="#" class="menu__link link">ФОТО</a></li>
                 </ul>
             </nav>
             <div class="header__additionally-menu menu">
                 <ul class="menu__list">
-                    <li class="menu__item menu__item--link"><a href="//www.3x3planet.com/" target="_blank" class="menu__link link">РЕГИСТРАЦИЯ КОМАНДЫ</a></li>
-                    <li class="menu__item menu__item--link"><a href="#become_volunteer_form" class="menu__link link become_volunteer_form">СТАТЬ ВОЛОНТЕРОМ</a></li>
-                    <li class="menu__item menu__item--link"><a href="#become_sponsor_form" class="menu__link link become_sponsor_form">СТАТЬ СПОНСОРОМ</a></li>
+                    <li class="menu__item menu__item--link"><a href="//www.3x3planet.com/" target="_blank" class="menu__link yellow-link">РЕГИСТРАЦИЯ КОМАНДЫ</a></li>
+                    <li class="menu__item menu__item--link"><a href="#become_volunteer_form" class="menu__link yellow-link become_volunteer_form">СТАТЬ ВОЛОНТЕРОМ</a></li>
+                    <li class="menu__item menu__item--link"><a href="#become_sponsor_form" class="menu__link yellow-link become_sponsor_form">СТАТЬ СПОНСОРОМ</a></li>
                 </ul>
             </div>
         </div>
