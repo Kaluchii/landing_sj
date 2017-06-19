5a5eb4ba0057a3bf6c2c5799d0d086b971c091c1
kaluchii <mr.kaluchii@mail.ru>
2017-06-19 12:16:38 +0600

    Change base site color

--- resources/views/front/index/index.blade.php
+++ resources/views/front/index/index.blade.php
@@ -32,7 +32,7 @@
             </div>
         </div>
         <h1 class="second-header__block-title">
-            {!! wrap_in_tag($titular->block_title_field, '<span class="thin-green">', '</span>') !!}
+            {!! wrap_in_tag($titular->block_title_field, '<span class="thin-colored">', '</span>') !!}
         </h1>
     </section>
 
@@ -48,7 +48,7 @@
             </div>
             <ul class="titular__facts facts">
                 @foreach($titular->facts_group as $item)
-                    <li class="facts__item">{!! wrap_in_tag($item->text_field, '<span class="reg-green">', '</span>') !!}</li>
+                    <li class="facts__item">{!! wrap_in_tag($item->text_field, '<span class="reg-colored">', '</span>') !!}</li>
                 @endforeach
             </ul>
         </div>
@@ -58,7 +58,7 @@
     <section class="statistic">
         <div class="statistic__wrap-1090">
             <h2 class="statistic__block-title block-title">
-                {!! wrap_in_tag($statistic->block_title_field, '<span class="thin-green">', '</span>') !!}
+                {!! wrap_in_tag($statistic->block_title_field, '<span class="thin-colored">', '</span>') !!}
             </h2>
             <div class="statistic__img-facts-wrap">
                 <div class="statistic__img-wrap">
@@ -83,14 +83,14 @@
     <section class="principles">
         <div class="principles__wrap-1080">
             <h2 class="principles__block-title block-title">
-                {!! wrap_in_tag($principles->block_title_field, '<span class="thin-green">', '</span>') !!}
+                {!! wrap_in_tag($principles->block_title_field, '<span class="thin-colored">', '</span>') !!}
             </h2>
             <ul class="principles__list three-elem-list">
                 @foreach($principles->principles_list_group as $item)
                     <li class="three-elem-list__item info-block info-block--with-img">
                         <img src="{{$item->img_field->link}}" alt="{{$item->img_field->alt}}" class="info-block__img info-block__img--move">
                         <div class="info-block__text-wrap">
-                            <h3 class="info-block__title">{!! wrap_in_tag($item->item_title_field, '<span class="reg-green">', '</span>') !!}</h3>
+                            <h3 class="info-block__title">{!! wrap_in_tag($item->item_title_field, '<span class="reg-colored">', '</span>') !!}</h3>
                             <p class="info-block__text">{!! $item->text_field !!}</p>
                         </div>
                     </li>
@@ -103,14 +103,14 @@
     <section class="objectives">
         <div class="objectives__wrap-1180">
             <h2 class="objectives__block-title block-title">
-                {!! wrap_in_tag($objectives->block_title_field, '<span class="thin-green">', '</span>') !!}
+                {!! wrap_in_tag($objectives->block_title_field, '<span class="thin-colored">', '</span>') !!}
             </h2>
             <div class="objectives__img-targets-wrap">
                 <img src="{{$objectives->main_img_field->link}}" alt="{{$objectives->main_img_field->alt}}" class="objectives__img">
                 <ul class="objectives__list targets-list">
                     @foreach($objectives->objectives_list_group as $item)
                         <li class="targets-list__item info-block">
-                            <h3 class="info-block__title">{!! wrap_in_tag($item->item_title_field, '<span class="reg-green">', '</span>') !!}</h3>
+                            <h3 class="info-block__title">{!! wrap_in_tag($item->item_title_field, '<span class="reg-colored">', '</span>') !!}</h3>
                             <p class="info-block__text">{!! $item->text_field !!}</p>
                         </li>
                     @endforeach
@@ -123,14 +123,14 @@
     <section class="leaders">
         <div class="leaders__wrap-1080">
             <h2 class="leaders__block-title block-title">
-                {!! wrap_in_tag($leaders->block_title_field, '<span class="thin-green">', '</span>') !!}
+                {!! wrap_in_tag($leaders->block_title_field, '<span class="thin-colored">', '</span>') !!}
             </h2>
             <ul class="leaders__list three-elem-list">
                 @foreach($leaders->leaders_list_group as $item)
                     <li class="three-elem-list__item info-block info-block--with-img">
                         <img src="{{$item->img_field->link}}" alt="{{$item->img_field->alt}}" class="info-block__img info-block__img--move">
                         <div class="info-block__text-wrap">
-                            <h3 class="info-block__title">{!! wrap_in_tag($item->item_title_field, '<span class="reg-green">', '</span>') !!}</h3>
+                            <h3 class="info-block__title">{!! wrap_in_tag($item->item_title_field, '<span class="reg-colored">', '</span>') !!}</h3>
                             <p class="info-block__text">{!! $item->text_field !!}</p>
                         </div>
                     </li>
@@ -143,7 +143,7 @@
     <section class="support">
         <div class="support__wrap-1200">
             <h2 class="support__block-title block-title">
-                {!! wrap_in_tag($support->block_title_field, '<span class="thin-green">', '</span>') !!}
+                {!! wrap_in_tag($support->block_title_field, '<span class="thin-colored">', '</span>') !!}
             </h2>
             <ul class="support__images-list">
                 @foreach($support->support_list_group as $item)
@@ -158,12 +158,12 @@
         <div class="plans__wrap-1210">
             <div class="plans__text-col">
                 <h2 class="plans__block-title block-title">
-                    {!! wrap_in_tag($plans->block_title_field, '<span class="thin-green">', '</span>') !!}
+                    {!! wrap_in_tag($plans->block_title_field, '<span class="thin-colored">', '</span>') !!}
                 </h2>
                 <ul class="plans__list two-col-list">
                     @foreach($plans->plans_list_group as $item)
                         <li class="two-col-list__item info-block">
-                            <h3 class="info-block__title">{!! wrap_in_tag($item->item_title_field, '<span class="reg-green">', '</span>') !!}</h3>
+                            <h3 class="info-block__title">{!! wrap_in_tag($item->item_title_field, '<span class="reg-colored">', '</span>') !!}</h3>
                             <p class="info-block__text">{!! $item->text_field !!}</p>
                         </li>
                     @endforeach
@@ -178,7 +178,7 @@
         <div class="information__wrap-1160">
             <div class="information__text-wrap">
                 <h2 class="information__block-title block-title">
-                    {!! wrap_in_tag($information->block_title_field, '<span class="thin-green">', '</span>') !!}
+                    {!! wrap_in_tag($information->block_title_field, '<span class="thin-colored">', '</span>') !!}
                 </h2>
                 <div class="information__text">{!! $information->text_field !!}</div>
                 <div class="information__give-offer-wrap">
@@ -205,7 +205,7 @@
                     @else
                         <h2 class="package__block-title block-title">
                     @endif
-                        {!! wrap_in_tag($package->block_title_field, '<span class="thin-green">', '</span>') !!}
+                        {!! wrap_in_tag($package->block_title_field, '<span class="thin-colored">', '</span>') !!}
                     </h2>
                     <ul class="package__components-list components-list">
                         @for($i = 1; $i <=5; $i++)
@@ -214,19 +214,19 @@
                                 <li class="components-list__item info-block info-block--with-img">
                                     <img src="{{$item->img_field->link}}" alt="{{$item->img_field->alt}}" class="info-block__img info-block__img--move">
                                     <div class="info-block__text-wrap">
-                                        <h3 class="info-block__title">{{ $i }}.<br>{!! wrap_in_tag($item->item_title_field, '<span class="reg-green">', '</span>') !!}</h3>
+                                        <h3 class="info-block__title">{{ $i }}.<br>{!! wrap_in_tag($item->item_title_field, '<span class="reg-colored">', '</span>') !!}</h3>
                                         <p class="info-block__text">{{ $item->text_field  }}</p>
                                     </div>
                                 </li>
                             @else
                                 <li class="components-list__item @if($i == 4) components-list__item--520 @else components-list__item--490 @endif info-block">
                                     <img src="{{$item->img_field->link}}" alt="{{$item->img_field->alt}}" class="info-block__img">
-                                    <h3 class="info-block__title">{{ $i }}.<br>{!! wrap_in_tag($item->item_title_field, '<span class="reg-green">', '</span>') !!}</h3>
+                                    <h3 class="info-block__title">{{ $i }}.<br>{!! wrap_in_tag($item->item_title_field, '<span class="reg-colored">', '</span>') !!}</h3>
                                     <p class="info-block__text">{{ $item->text_field  }}</p>
                                     @if( $i == 5 )
                                         <?php $package->components_list_group->next(); ?>
                                         <?php $item = $package->components_list_group->current(); ?>
-                                        <h3 class="info-block__title info-block__title--top-indent">6.<br>{!! wrap_in_tag($item->item_title_field, '<span class="reg-green">', '</span>') !!}</h3>
+                                        <h3 class="info-block__title info-block__title--top-indent">6.<br>{!! wrap_in_tag($item->item_title_field, '<span class="reg-colored">', '</span>') !!}</h3>
                                         <p class="info-block__text">{{ $item->text_field  }}</p>
                                     @endif
                                 </li>
@@ -245,7 +245,7 @@
         <div class="contact-us__wrap-520">
             <div class="contact-us__contact-block contact-block">
                 <h3 class="contact-block__title">
-                    {!! wrap_in_tag($contact_us->block_title_field, '<span class="thin-green">', '</span>') !!}
+                    {!! wrap_in_tag($contact_us->block_title_field, '<span class="thin-colored">', '</span>') !!}
                 </h3>
                 <div class="contact-block__img-wrap">
                     <img src="{{$contact_us->main_img_field->link}}" alt="{{$contact_us->main_img_field->alt}}" class="contact-block__img">
